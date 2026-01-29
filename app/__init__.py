079fe7af9ced07543c623da67da5b993006a5c33
chandana28c <chandanalr7899@gmail.com>
2026-01-29 13:11:56 +0530

    Configure MongoDB for Flask app
    Added MongoDB configuration and initialization.

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ from app.webhook.routes import webhook
 
 # Creating our flask app
 def create_app():
-
     app = Flask(__name__)
-    
-    # registering all the blueprints
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/github_events"
+
+    from app.extensions import mongo
+    mongo.init_app(app)
+
     app.register_blueprint(webhook)
-    
     return app
